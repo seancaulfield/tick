bbe45c5b0de3324ebe2878b14bbc5bd2a8d244b8
Sean Caulfield <sean@yak.net>
2016-02-07 19:20:20 -0500

    out of fucks for today

--- firmware/wifi_tick/wifi_tick.ino
+++ firmware/wifi_tick/wifi_tick.ino
@@ -3,10 +3,18 @@
  *
  */
 
+#include <Wire.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
 #include <FS.h>
 #include <LedControl.h>
+
+#include <RTClib.h>
+#include <DS3231.h>
+#include <Time.h>
+#include <Timezone.h>
+#include <NTPClient.h>
+
 #include <string.h>
 
 #include "debug.h"
@@ -14,10 +22,11 @@
 
 // Pin assignments
 
-#define DISP_DATA 13
-#define DISP_CLCK 14
-#define DISP_LOAD 16
-#define DISP_NUM  1
+#define DISP_DATA       13
+#define DISP_CLCK       14
+#define DISP_LOAD       16
+#define DISP_NUM        1
+#define DISP_BRIGHTNESS 8
 
 //
 // TODO Define these for yourself!
@@ -30,6 +39,8 @@ const char *MY_WIFI_AP_KEY  = "<KEY>";
 //
 
 LedControl display = LedControl(DISP_DATA, DISP_CLCK, DISP_LOAD, DISP_NUM);
+RTC_DS3231 rtc = RTC_DS3231();
+NTPClient ntpclient;
 
 void setup_serial() {
   delay(200);
@@ -77,55 +88,62 @@ void setup() {
 
   }
 
+  //
+  // Start I2C & RTC
+  //
+  Wire.begin();
+  rtc.begin();
+  rtc.clearControlRegisters();
+
+  //
+  // Setup display(s)
+  //
+
+  for (int i=0; i<DISP_NUM; i++) {
+    display.shutdown(i, false);
+    display.setIntensity(i, DISP_BRIGHTNESS);
+    display.clearDisplay(0);
+  }
+
+  //
+  // Connect to Wifi
+  //
+
+  connect_wifi();
+
   delay(500);
 
 }
 
-#if 0
-void tcp_echo() {
-  const uint16_t port = 8081;
-  const char *hostname = "athena.lan";
-
-  DPRINT("Connecting to ");
-  DPRINTLN(hostname);
-
-  WiFiClient client;
-  if (!client.connect(hostname, port)) {
-    DPRINTLN("Connection failed!");
-    return;
-
-  }
-
-  // GREETINGS EARTHLING
-  DPRINT("> HAI");
-  client.println("HAI");
-
-  bool failure = false;
-  uint32_t timeout = 30 * 1000 + millis(); // 30 second timeout
-  while (!client.available()) {
-    if (timeout - millis() < 0) {
-      DPRINTLN("Timeout waiting for client");
-      failure = true;
-    }
-  }
-
-  if (!failure) {
-    while (client.available()) {
-      String stuff = client.readStringUntil('\n');
-      client.println(stuff);
-      DPRINT("> ");
-      DPRINTLN(stuff);
-    }
-  }
-
-  DPRINTLN("Closing...");
-  client.stop();
-
-}
-#endif
-
 void loop() {
-  //tcp_echo();
-  delay(5000);
+
+  for (int i=0; i<10; i++) {
+    display.clearDisplay(0);
+    display.setDigit(0, 0, i, true);
+    delay(500);
+  }
+
+  // Has its own internal check to only poll every 60s
+  ntpclient.update();
+
+  uint32_t ntp_time = ntpclient.getRawTime();
+  uint32_t rtc_time = rtc.now().unixtime();
+
+  DateTime ntp_dt = DateTime(ntp_time);
+  DateTime rtc_dt = DateTime(rtc_time);
+
+  // Check if RTC is far enough off to require adjustment
+  if (rtc_time > ntp_time || ntp_time - rtc_time > 60) {
+    rtc.adjust(ntp_dt);
+  }
+
+  // Fart out timestamp
+  char buff[64];
+  ntp_dt.toString(buff, sizeof(buff));
+  DPRINT(buff);
+  DPRINT(" ");
+  rtc_dt.toString(buff, sizeof(buff));
+  DPRINTLN(buff);
+
 }
 
