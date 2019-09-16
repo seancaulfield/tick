6ca0f7e0f75e98e17120d67d5d17d418e97954e8
Sean Caulfield <sean@yak.net>
2019-09-15 21:20:13 -0400

    authorized_keys file to right dir derp

--- firmware/rpi_setup_sd.sh
+++ firmware/rpi_setup_sd.sh
@@ -9,25 +9,26 @@ RPIWIFIPASS=
 
 # Bail if error happens
 set -o errexit
+set -o verbose
 
 # Should set this but...
-sudo vim ${RPIBOOT}/config.txt
 sudo tee -a ${RPIBOOT}/config.txt <<EOF
 dtoverlay=i2c-rtc,ds1307
-dtoverlay=gpio-poweroff,gpiopin=24
-dtoverlay=gpio-shutdown,gpiopin=23
+dtoverlay=gpio-poweroff,gpio_pin=24
+dtoverlay=gpio-shutdown,gpio_pin=23,gpio_pull=2,active_low=1
 EOF
+sudo vim ${RPIBOOT}/config.txt
 
 # Enable SSH support on first boot
 sudo touch ${RPIBOOT}/ssh
 
 # Set hostname
-echo "${RPIHOSTNAME}" > ${RPIROOT}/etc/hostname
+echo "${RPIHOSTNAME}" | sudo tee ${RPIROOT}/etc/hostname
 
 # Enable login via SSH key by adding our pubkey to ~/.authorized_keys
 sudo mkdir -m 700 ${RPIROOT}/home/pi/.ssh/
 sudo chown 1000:1000 ${RPIROOT}/home/pi/.ssh/
-cat ${RPISSHKEY} | sudo tee -a ${RPIROOT}/home/pi/authorized_keys
+cat ${RPISSHKEY} | sudo tee -a ${RPIROOT}/home/pi/.ssh/authorized_keys
 
 # Disable password login for pi user
 sudo tee -a ${RPIROOT}/etc/ssh/sshd_config <<EOF
@@ -42,4 +43,14 @@ network={
   ssid="${RPIWIFISSID}"
   psk="${RPIWIFIPASS}"
 }
+EOF
+
+# Copy clock program over
+sudo install -m 0755 -o root -g root -t ${RPIROOT}/usr/local/bin/ rpi_ht16k33/clock.py
+
+# And add to rc.local
+sudo sed -ie "s/exit 0/sudo -u pi \/usr\/local\/bin\/clock.py/" ${RPIROOT}/etc/rc.local
+sudo tee -a ${RPIROOT}/etc/rc.local <<EOF
+
+exit 0
 EOF
